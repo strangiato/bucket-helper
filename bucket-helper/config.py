87454b145fd4ad77361ff73a14bc135bfa561042
Trevor Royer <troyer@redhat.com>
2021-01-27 14:53:56 -0600

    update config

--- bucket-helper/config.py
+++ bucket-helper/config.py
@@ -17,9 +17,24 @@ class AppConfig:
         endpoint_url = environ.var(
             help="The S3 endpoint URL that bucket-helper will connect to"
         )
-        bucket = environ.var(
-            help="The bucket in the S3 instance that will be created",
-        )
+
+        @environ.config
+        class Bucket:
+
+            name = environ.var(
+                help="The name of the bucket in the S3 instance that will be created",
+            )
+            public = environ.bool_var(False, help="Set the access policy for the bucket to public as a boolean value")
+            
+            @environ.config
+            class Topic:
+                name = environ.var(None, help="Creates a notification topic for the bucket")
+                push_endpoint = environ.var(None, help="The endpoint URL that the topic will push to")
+            
+            topic = environ.group(Topic)
+
+        bucket = environ.group(Bucket)
+
         access_key = environ.var(
             name="AWS_ACCESS_KEY_ID", help="The Access Key for the S3 instance"
         )
