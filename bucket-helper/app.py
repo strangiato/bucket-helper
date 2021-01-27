38ecf795e6755db6a75eaa9eb1fa6988cca73b2a
Trevor Royer <troyer@redhat.com>
2021-01-27 16:11:30 -0600

    create app logic

---
+++ bucket-helper/app.py
@@ -0,0 +1,151 @@
+"""Application used for managing and configuring s3 buckets."""
+
+import json
+import logging
+import sys
+
+import boto3
+
+import botocore
+
+from config import AppConfig
+
+from dotenv import load_dotenv
+
+import environ
+
+# load environment and assign it to the environ config
+load_dotenv()
+app_cfg = environ.to_config(AppConfig)
+
+# setup logging
+logger = logging.getLogger(__name__)
+
+logger.setLevel(app_cfg.log.level)
+formatter = logging.Formatter(app_cfg.log.formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+
+
+def create_bucket(s3, bucket_name):
+    """Create the bucket and return the bucket object."""
+    try:
+        bucket = s3.create_bucket(Bucket=bucket_name)
+        logger.info(f"Created bucket {bucket_name}")
+    except Exception:
+        logger.exception(
+            f"Unable to create the bucket '{bucket_name}' on the S3 instance"
+        )
+        sys.exit(1)
+
+    return bucket
+
+
+def set_public_policy(s3, bucket):
+    """Set the bucket read access to public."""
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AddPerm",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": ["s3:GetObject"],
+                "Resource": ["arn:aws:s3:::{0}/*".format(bucket["Name"])],
+            }
+        ],
+    }
+    bucket_policy = json.dumps(bucket_policy)
+
+    try:
+        s3.put_bucket_policy(Bucket=bucket["Name"], Policy=bucket_policy)
+        logger.info(f"Updated bucket policy to public for {bucket['Name']}")
+
+    except Exception:
+        logger.exception(f"Unable to set public policy to bucket {bucket['Name']}")
+        sys.exit(1)
+
+    return bucket_policy
+
+
+def create_topic(sns, bucket, topic, push_endpoint=None):
+    """Create a topic for the bucket and configure the push events."""
+    attributes = {}
+    if push_endpoint:
+        attributes["push-endpoint"] = push_endpoint
+        attributes["kafka-ack-level"] = "broker"
+
+    try:
+        topic_arn = sns.create_topic(Name=topic, Attributes=attributes)["TopicArn"]
+        logger.info(f"Created topic {topic}")
+
+    except Exception:
+        logger.exception(f"Unable to create topic {topic}")
+        sys.exit(1)
+
+    bucket_notifications_configuration = {
+        "TopicConfigurations": [
+            {
+                "Id": f"{topic}",
+                "TopicArn": f"arn:aws:sns:s3a::{topic}",
+                "Events": ["s3:ObjectCreated:*"],
+            }
+        ]
+    }
+
+    try:
+        s3.put_bucket_notification_configuration(
+            Bucket=bucket["Name"],
+            NotificationConfiguration=bucket_notifications_configuration,
+        )
+        logger.info(
+            f"Created notification for bucket {bucket['Name']} on topic {topic}"
+        )
+
+    except Exception:
+        logger.exception(
+            f"Unable to create notification for bucket {bucket['Name']} on topic {topic}"
+        )
+        sys.exit(1)
+
+    return topic_arn
+
+
+if __name__ == "__main__":
+
+    logger.info(f"S3 endpoint URL: {app_cfg.s3.endpoint_url}")
+
+    s3 = boto3.resource(
+        "s3",
+        endpoint_url=app_cfg.s3.endpoint_url,
+        aws_access_key_id=app_cfg.s3.access_key,
+        aws_secret_access_key=app_cfg.s3.secret_key,
+        region_name="default",
+        config=botocore.client.Config(signature_version="s3"),
+    )
+
+    bucket = create_bucket(s3, app_cfg.s3.bucket.name)
+
+    if app_cfg.s3.bucket.policy:
+        set_public_policy(s3, bucket)
+
+    if app_cfg.s3.bucket.topic.name:
+
+        sns = boto3.client(
+            "sns",
+            endpoint_url=app_cfg.s3.endpoint_url,
+            aws_access_key_id=app_cfg.s3.access_key,
+            aws_secret_access_key=app_cfg.s3.secret_key,
+            region_name="default",
+            config=botocore.client.Config(signature_version="s3"),
+        )
+
+        create_topic(
+            sns,
+            bucket,
+            app_cfg.s3.bucket.topic,
+            push_endpoint=app_cfg.s3.bucket.topic.push_endpoint,
+        )
--- bucket-helper/config.py
+++ bucket-helper/config.py
@@ -1,12 +1,18 @@
+"""Configuration file for managing environment variables."""
+
 import environ
 
 
 @environ.config()
 class AppConfig:
+    """Application configuration object used for managing environment variables."""
+
     @environ.config
     class Log:
+        """App configuration object used for managing logging settings."""
+
         level = environ.var("INFO")
-        format = environ.var(
+        formatter = environ.var(
             "%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
         )
 
@@ -14,23 +20,35 @@ class AppConfig:
 
     @environ.config
     class S3:
+        """App configuration object used for managing s3 objects."""
+
         endpoint_url = environ.var(
             help="The S3 endpoint URL that bucket-helper will connect to"
         )
 
         @environ.config
         class Bucket:
+            """App configuration object used for managing s3 bucket settings."""
 
             name = environ.var(
                 help="The name of the bucket in the S3 instance that will be created",
             )
-            public = environ.bool_var(False, help="Set the access policy for the bucket to public as a boolean value")
-            
+            public = environ.bool_var(
+                False,
+                help="Set the access policy for the bucket to public as a boolean value",
+            )
+
             @environ.config
             class Topic:
-                name = environ.var(None, help="Creates a notification topic for the bucket")
-                push_endpoint = environ.var(None, help="The endpoint URL that the topic will push to")
-            
+                """App configuration object used for managing topic configurations for an s3 bucket."""
+
+                name = environ.var(
+                    None, help="Creates a notification topic for the bucket"
+                )
+                push_endpoint = environ.var(
+                    None, help="The endpoint URL that the topic will push to"
+                )
+
             topic = environ.group(Topic)
 
         bucket = environ.group(Bucket)
